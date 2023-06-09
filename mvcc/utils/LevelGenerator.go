5fa85c729e78e163830344eadb1cbd2881435876
Sarthak Makhija <sarthak.makhija@gmail.com>
2023-06-09 18:51:34 +0530

    Sarthak | Removes deprecated Seed() from LevelGenerator

--- mvcc/utils/LevelGenerator.go
+++ mvcc/utils/LevelGenerator.go
@@ -8,19 +8,22 @@ import (
 type LevelGenerator struct {
 	maxLevel   int
 	skipFactor int
+	random     *rand.Rand
 }
 
 func NewLevelGenerator(maxLevel int) LevelGenerator {
-	rand.Seed(time.Now().UnixNano())
+	random := rand.New(rand.NewSource(time.Now().UnixNano()))
+
 	return LevelGenerator{
 		maxLevel:   maxLevel,
 		skipFactor: 2,
+		random:     random,
 	}
 }
 
 func (levelGenerator LevelGenerator) Generate() int {
 	level := 1
-	newRandom := rand.Float64()
+	newRandom := levelGenerator.random.Float64()
 	for level < levelGenerator.GetMaxLevel() && newRandom < 1.0/float64(levelGenerator.skipFactor) {
 		level = level + 1
 		newRandom = rand.Float64()
--- mvcc/utils/LevelGenerator_test.go
+++ mvcc/utils/LevelGenerator_test.go
@@ -10,3 +10,14 @@ func TestShouldGenerateLevelGreaterThanEqualTo1(t *testing.T) {
 		t.Fatalf("Expected generated level to be greater than or equal to 1 but received %v", level)
 	}
 }
+
+func TestShouldGenerateLevelGreaterThanEqualTo1_MultipleRuns(t *testing.T) {
+	levelGenerator := NewLevelGenerator(10)
+	for count := 1; count <= 1000; count++ {
+		level := levelGenerator.Generate()
+
+		if level < 1 {
+			t.Fatalf("Expected generated level to be greater than or equal to 1 but received %v", level)
+		}
+	}
+}
