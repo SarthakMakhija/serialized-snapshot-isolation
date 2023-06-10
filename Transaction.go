509ee77a24aa84c7593f2f44cecb0b880f4681f9
Sarthak Makhija <sarthak.makhija@gmail.com>
2023-06-10 12:59:14 +0530

    Sarthak | Renames Transaction to ReadonlyTransaction

--- Transaction.go
+++ Transaction.go
@@ -2,23 +2,20 @@ package snapshot_isolation
 
 import "snapshot-isolation/mvcc"
 
-type Transaction struct {
-	isReadonly    bool
+type ReadonlyTransaction struct {
 	readTimestamp uint64
-
 	//TODO: will change later
 	memtable *mvcc.MemTable
 }
 
-func NewReadonlyTransaction(readTimestamp uint64, memtable *mvcc.MemTable) *Transaction {
-	return &Transaction{
-		isReadonly:    true,
+func NewReadonlyTransaction(readTimestamp uint64, memtable *mvcc.MemTable) *ReadonlyTransaction {
+	return &ReadonlyTransaction{
 		readTimestamp: readTimestamp,
 		memtable:      memtable,
 	}
 }
 
-func (transaction *Transaction) Get(key []byte) (mvcc.Value, bool) {
+func (transaction *ReadonlyTransaction) Get(key []byte) (mvcc.Value, bool) {
 	//TODO: If the transaction is not readonly, we need to capture the keys in the readSet
 	versionedKey := mvcc.NewVersionedKey(key, transaction.readTimestamp)
 	return transaction.memtable.Get(versionedKey)
